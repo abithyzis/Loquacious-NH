f9c971ac6963395803827bc7810c77f7981bb9c5
Scott Findlater <scottfindlater@gmail.com>
2011-08-17 22:33:38 +0100

    updated nhibernate configs

--- Test.NHByCode/OutputsSql/schema.sql
+++ Test.NHByCode/OutputsSql/schema.sql
@@ -7,8 +7,12 @@ alter table Customer_Order  drop constraint FK13A0EC01B96725C
 alter table Customer_Order  drop constraint FK_Customer_Order
 
 
-    if exists (select 1 from sys.objects where object_id = OBJECT_ID(N'[FKD1436656AFB5CCE4]') AND parent_object_id = OBJECT_ID('[Order]'))
-alter table [Order]  drop constraint FKD1436656AFB5CCE4
+    if exists (select 1 from sys.objects where object_id = OBJECT_ID(N'[FK_Product_Category_ProductId]') AND parent_object_id = OBJECT_ID('Product_Category'))
+alter table Product_Category  drop constraint FK_Product_Category_ProductId
+
+
+    if exists (select 1 from sys.objects where object_id = OBJECT_ID(N'[FK_Product_Category_CategoryId]') AND parent_object_id = OBJECT_ID('Product_Category'))
+alter table Product_Category  drop constraint FK_Product_Category_CategoryId
 
 
     if exists (select 1 from sys.objects where object_id = OBJECT_ID(N'[FK59090D7B1BAFC1F2]') AND parent_object_id = OBJECT_ID('OrderItem'))
@@ -23,31 +27,27 @@ alter table OrderItem  drop constraint FK59090D7B39506BFC
 alter table OrderItem  drop constraint FK_Order_OrderItem
 
 
+    if exists (select 1 from sys.objects where object_id = OBJECT_ID(N'[FKD1436656AFB5CCE4]') AND parent_object_id = OBJECT_ID('[Order]'))
+alter table [Order]  drop constraint FKD1436656AFB5CCE4
+
+
     if exists (select 1 from sys.objects where object_id = OBJECT_ID(N'[FKFA88A7FAEE84E9A8]') AND parent_object_id = OBJECT_ID('Category'))
 alter table Category  drop constraint FKFA88A7FAEE84E9A8
-
-
-    if exists (select 1 from sys.objects where object_id = OBJECT_ID(N'[FK_Product_Category_CategoryId]') AND parent_object_id = OBJECT_ID('Product_Category'))
-alter table Product_Category  drop constraint FK_Product_Category_CategoryId
-
-
-    if exists (select 1 from sys.objects where object_id = OBJECT_ID(N'[FK_Product_Category_ProductId]') AND parent_object_id = OBJECT_ID('Product_Category'))
-alter table Product_Category  drop constraint FK_Product_Category_ProductId
 
 
     if exists (select * from dbo.sysobjects where id = object_id(N'Customer') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table Customer
 
     if exists (select * from dbo.sysobjects where id = object_id(N'Customer_Order') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table Customer_Order
 
-    if exists (select * from dbo.sysobjects where id = object_id(N'[Order]') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table [Order]
-
-    if exists (select * from dbo.sysobjects where id = object_id(N'OrderItem') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table OrderItem
-
-    if exists (select * from dbo.sysobjects where id = object_id(N'Category') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table Category
+    if exists (select * from dbo.sysobjects where id = object_id(N'Product') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table Product
 
     if exists (select * from dbo.sysobjects where id = object_id(N'Product_Category') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table Product_Category
 
-    if exists (select * from dbo.sysobjects where id = object_id(N'Product') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table Product
+    if exists (select * from dbo.sysobjects where id = object_id(N'OrderItem') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table OrderItem
+
+    if exists (select * from dbo.sysobjects where id = object_id(N'[Order]') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table [Order]
+
+    if exists (select * from dbo.sysobjects where id = object_id(N'Category') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table Category
 
     create table Customer (
         CustomerId UNIQUEIDENTIFIER not null,
@@ -62,11 +62,17 @@ alter table Product_Category  drop constraint FK_Product_Category_ProductId
        primary key (OrderId, CustomerId)
     )
 
-    create table [Order] (
-        OrderId UNIQUEIDENTIFIER not null,
-       CustomerId UNIQUEIDENTIFIER not null,
-       OrderDate DATETIME null,
-       primary key (OrderId)
+    create table Product (
+        ProductId UNIQUEIDENTIFIER not null,
+       Name NVARCHAR(450) null,
+       Description NVARCHAR(MAX) null,
+       primary key (ProductId)
+    )
+
+    create table Product_Category (
+        CategoryId UNIQUEIDENTIFIER not null,
+       ProductId UNIQUEIDENTIFIER not null,
+       primary key (ProductId, CategoryId)
     )
 
     create table OrderItem (
@@ -78,25 +84,19 @@ alter table Product_Category  drop constraint FK_Product_Category_ProductId
        primary key (OrderItemId)
     )
 
+    create table [Order] (
+        OrderId UNIQUEIDENTIFIER not null,
+       CustomerId UNIQUEIDENTIFIER not null,
+       OrderDate DATETIME null,
+       primary key (OrderId)
+    )
+
     create table Category (
         CategoryId UNIQUEIDENTIFIER not null,
        Name NVARCHAR(450) null,
        Description NVARCHAR(2000) null,
        ParentCategoryId UNIQUEIDENTIFIER null,
        primary key (CategoryId)
-    )
-
-    create table Product_Category (
-        ProductId UNIQUEIDENTIFIER not null,
-       CategoryId UNIQUEIDENTIFIER not null,
-       primary key (CategoryId, ProductId)
-    )
-
-    create table Product (
-        ProductId UNIQUEIDENTIFIER not null,
-       Name NVARCHAR(450) null,
-       Description NVARCHAR(MAX) null,
-       primary key (ProductId)
     )
 
     alter table Customer_Order 
@@ -109,10 +109,15 @@ alter table Product_Category  drop constraint FK_Product_Category_ProductId
         foreign key (OrderId) 
         references Customer
 
-    alter table [Order] 
-        add constraint FKD1436656AFB5CCE4 
-        foreign key (CustomerId) 
-        references Customer
+    alter table Product_Category 
+        add constraint FK_Product_Category_ProductId 
+        foreign key (ProductId) 
+        references Category
+
+    alter table Product_Category 
+        add constraint FK_Product_Category_CategoryId 
+        foreign key (CategoryId) 
+        references Product
 
     alter table OrderItem 
         add constraint FK59090D7B1BAFC1F2 
@@ -129,17 +134,12 @@ alter table Product_Category  drop constraint FK_Product_Category_ProductId
         foreign key (OrderItemId) 
         references [Order]
 
+    alter table [Order] 
+        add constraint FKD1436656AFB5CCE4 
+        foreign key (CustomerId) 
+        references Customer
+
     alter table Category 
         add constraint FKFA88A7FAEE84E9A8 
         foreign key (ParentCategoryId) 
-        references Category
-
-    alter table Product_Category 
-        add constraint FK_Product_Category_CategoryId 
-        foreign key (CategoryId) 
-        references Product
-
-    alter table Product_Category 
-        add constraint FK_Product_Category_ProductId 
-        foreign key (ProductId) 
         references Category
